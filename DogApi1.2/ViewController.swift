25b2f0dcbb1ab777ca8a1f511b3b83dc97f81e6e
Francisco Escobar <91104756+fran13emCambrian@users.noreply.github.com>
2022-03-13 17:32:41 -0230

    Initial Commit

---
+++ DogApi1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogApi1.2
+//
+//  Created by Francisco Escobar on 2022-03-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DogApi1.2UITests/DogApi1_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DogApi1_2UITestsLaunchTests.swift
+//  DogApi1.2UITests
+//
+//  Created by Francisco Escobar on 2022-03-13.
+//
+
+import XCTest
+
+class DogApi1_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
